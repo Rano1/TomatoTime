bf1df632a269bebfe73049461c20df16bf24e13d
Ra <myfuture@vip.qq.com>
2014-05-16 09:09:02 +0800

    Revert "update"
    This reverts commit 59c915abda0a0304ff521433ffb793c013b82450.

--- TomatoTime/gen/com/time/tomato/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.time.tomato;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- TomatoTime/src/com/time/tomato/base/BaseActivity.java
+++
@@ -1,7 +0,0 @@
-package com.time.tomato.base;
-
-import android.app.Activity;
-
-public class BaseActivity extends Activity{
-
-}
